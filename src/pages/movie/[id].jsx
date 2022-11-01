7988aacf4de1a1b67f6ae4d82854af73bb52ca79
Geras4323 <german432yo@gmail.com>
2022-11-01 18:40:16 -0300

    [CLASE 15] 'Add to favourites' hearts

--- src/pages/movie/[id].jsx
+++ src/pages/movie/[id].jsx
@@ -48,7 +48,7 @@ export default function Detail() {
               <div className="relative top-0 h-160">
                 <img className="w-full h-full object-cover" src={`https://image.tmdb.org/t/p/w500${imagen}`} alt="" />
                 <div className="w-full h-full absolute top-0 flex flex-col gap-y-12 items-center bg-gradient-to-b from-transparent via-transparent to-slate-700   md:backdrop-blur-sm">
-                  <section className="w-full h-14 flex flex-row items-center justify-start p-2 bg-gradient-to-b from-slate-900 to-transparent bg-opacity-50">
+                  <section className="w-full h-14 flex flex-row items-center justify-between p-2 bg-gradient-to-b from-slate-900 to-transparent bg-opacity-50">
                     <Link href="/">
                       <section className="flex flex-row items-center gap-1   hover:cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className='w-10 h-8 fill-current text-gray-100'>
@@ -57,9 +57,13 @@ export default function Detail() {
                         <p className="text-xl text-gray-100 font-bold">Home</p>
                       </section>
                     </Link>
+                    <div className="mr-2 w-10 h-10 bg-black bg-opacity-60 rounded-full grid place-content-center z-10">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-6 h-6   cursor-pointer">
+                      <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z"/></svg>
+                    </div>
                   </section>
                   {imagen
-                    ? <img className="w-80 rounded-2xl border border-border" src={`https://image.tmdb.org/t/p/w500${imagen}`} alt={movie.name || movie.title} />
+                    ? <img className="w-80 rounded-2xl border border-border" src={`https://image.tmdb.org/t/p/w500${imagen}`} alt={movieDetails.name || movieDetails.title} />
                     : <img className="w-80 rounded-2xl border border-border bg-black bg-opacity-50" src={'/noimage.png'} alt="No image found" />
                   }
                 </div>
